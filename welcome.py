c3e57bd70756440756144d6cbc8a85170094b364
SugarClub <50675840+toosad@users.noreply.github.com>
2023-11-16 10:48:15 +0100

    Python Learning

---
+++ welcome.py
@@ -0,0 +1,12 @@
+
+line01 = "********************"  # header /Footer
+line02 = "*                  *"
+line03 = "*     Welcome!     *"
+
+# Starts with blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
